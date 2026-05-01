4394b781bc72edb97a88d406262221a339de7756
Anuraag Mishra <anurag.r.mishra234@gmail.com>
2026-05-01 14:34:03 +0530

    Update SW to bypass cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'unibuzz-v1';
+const CACHE_NAME = 'unibuzz-v2';
 const ASSETS_TO_CACHE = [
   '/index.html',
   '/style.css',
@@ -11,6 +11,17 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
   );
 });
 
